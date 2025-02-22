dee10cfbb442cad3a708e51b67a5397bc6898c5f
dljardim <dljardim@outlook.com>
2025-02-22 13:04:22 -0600

    added - button with an image image with a onTapgesture

--- BipTheGuy/ContentView.swift
+++ BipTheGuy/ContentView.swift
@@ -6,14 +6,62 @@
 //
 
 import SwiftUI
+import AVFAudio
 
 struct ContentView: View {
+    
+    @State private var audioPlayer: AVAudioPlayer!
+    private let soundName = "punchSound"
+    private let imageName = "clown"
+    
+    func playSound(soundName:String){
+        
+        if(audioPlayer != nil && audioPlayer.isPlaying){
+            audioPlayer.stop()
+        }
+        
+        // check if that file exists and can be played
+        guard let soundFile = NSDataAsset(name: soundName) else {
+            print("😡 Could not read file name \(soundName)")
+            return
+        }
+        
+        // play the sound
+        do{
+            audioPlayer = try AVAudioPlayer(data: soundFile.data)
+            audioPlayer.play()
+        }catch{
+            print("😡 ERROR: \(error.localizedDescription) creating audioPlayer")
+        }
+    }
+    
+    
+    func handleClick(){
+        playSound(soundName: soundName)
+    }
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Spacer()
+            
+            Button{
+                playSound(soundName: soundName)
+            }label:{
+                Image(imageName)
+                    .resizable()
+                    .scaledToFit()
+            }
+            
+            Spacer()
+            
+            Image(imageName)
+                .resizable()
+                .scaledToFit()
+                .onTapGesture {
+                    playSound(soundName: soundName)
+                }
+            
+            Spacer()
         }
         .padding()
     }
